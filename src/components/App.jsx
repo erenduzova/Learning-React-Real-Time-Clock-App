f844304067a561e13a1ae46e63068ee76882d129
erenduzova <90982361+erenduzova@users.noreply.github.com>
2022-06-17 19:04:06 +0300

    show time with button With useState and button every time button is clicked we show current time

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  let [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  function getTime() {
+    setTime((time = new Date().toLocaleTimeString()));
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
 //Challenge:
-//1. Given that you can get the current time using:
-let time = new Date().toLocaleTimeString();
-console.log(time);
-//Show the latest time in the <h1> when the Get Time button
-//is pressed.
 
 //2. Given that you can get code to be called every second
 //using the setInterval method.
